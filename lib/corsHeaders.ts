9c0da3f4e0d839c49b82f5b188ada40ea10b8088
Ravesss-dev04 <animeteevi@gmail.com>
2026-04-11 16:46:55 +0800

    asd23123d

--- lib/corsHeaders.ts
+++ lib/corsHeaders.ts
@@ -4,6 +4,7 @@ export const corsHeaders = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   'Access-Control-Allow-Credentials': 'true',
+  Vary: 'Origin',
   };
   
   
